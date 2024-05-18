fff6af5b6e2f3bc9bf10799fc10ff20133b7f6a2
EyEmilyKim surf <myue4555@gmail.com>
2024-05-18 19:49:34 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ archive/24200/1102_24263.js
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/24263
+/*
+알고리즘 수업 - 알고리즘의 수행 시간 2
+
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+
+출력>
+첫째 줄에 코드1 의 수행 횟수를 출력한다.
+둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+// 입력값 파싱
+const path = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
+const input = Number(require('fs').readFileSync(path).toString().trim());
+// console.log(input);
+
+// 문제 로직
+console.log(input);
+console.log(1);
