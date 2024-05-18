025d4c8449eb71cb88c0b1d15c56748100042a3f
EyEmilyKim surf <myue4555@gmail.com>
2024-05-18 14:10:47 +0900

    Hello World

--- NodeJs/0101_2557.js
+++
@@ -1,6 +0,0 @@
-// https://www.acmicpc.net/problem/2557
-/**
- * Hello World!를 출력하시오.
- */
-
-console.log('Hello World!');
---
+++ archive/02500/0101_2557.js
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/2557
+/*
+Hello World 
+
+Hello World!를 출력하시오.
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+console.log('Hello World!');
