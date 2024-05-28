f57a39f5114b8a2417f971984f10522f42276e79
EyEmilyKim surf <myue4555@gmail.com>
2024-05-28 14:39:13 +0900

    베라의 패션

---
+++ archive/15400/1901_15439.js
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/15439
+/*
+베라의 패션
+
+베라는 상의 N 벌과 하의 N 벌이 있다. i 번째 상의와 i 번째 하의는 모두 색상 i를 가진다. N 개의 색상은 모두 서로 다르다.
+상의와 하의가 서로 다른 색상인 조합은 총 몇 가지일까?
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+// 입력값 파싱
+const path = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
+const input = Number(require('fs').readFileSync(path).toString().trim());
+// console.log(input);
+
+// 문제 로직
+console.log(input ** 2 - input);
