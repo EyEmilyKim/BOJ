dfbc9193850aa76bd01f2e99792047df5e20e805
EyEmilyKim surf <myue4555@gmail.com>
2024-08-21 15:01:24 +0900

    일곱 난쟁이

---
+++ archive/02300/AlBas2_50001_2309.js
@@ -0,0 +1,32 @@
+// https://www.acmicpc.net/problem/2309
+/*
+일곱 난쟁이
+
+왕비를 피해 일곱 난쟁이들과 함께 평화롭게 생활하고 있던 백설공주에게 위기가 찾아왔다. 일과를 마치고 돌아온 난쟁이가 일곱 명이 아닌 아홉 명이었던 것이다.
+
+아홉 명의 난쟁이는 모두 자신이 "백설 공주와 일곱 난쟁이"의 주인공이라고 주장했다. 뛰어난 수학적 직관력을 가지고 있던 백설공주는, 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
+
+아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+// 입력값 파싱
+const path = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
+const [...height] = require('fs').readFileSync(path).toString().trim().split(/\r?\n/).map(Number);
+// console.log(height);
+
+// 문제 로직
+let seven = null;
+for (let i = 0; i < 9; i++) {
+  for (let j = i + 1; j < 9; j++) {
+    const sum = height.reduce((acc, cur) => acc + cur, 0);
+    if (sum - height[i] - height[j] === 100) {
+      seven = height.filter((el) => el !== height[i] && el !== height[j]);
+      break;
+    }
+  }
+  if (seven) break;
+}
+seven.sort((a, b) => a - b);
+console.log(seven.join('\n'));
