d0d0a6090c0f4ac780d18e886e90143495ae7622
EyEmilyKim surf <myue4555@gmail.com>
2024-12-27 00:59:14 +0900

    Rook

---
+++ archive/02300/2393.js
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/2393
+/*
+Rook
+
+You have just learned how to output text to the screen and your teacher has challenged you to create an ASCII art of a chess piece. You have decided to make your favorite piece, the rook.
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+console.log(`  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \\_________/
+    \\_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \\__
+ /             \\
+/_______________\\`);
