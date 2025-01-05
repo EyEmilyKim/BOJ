2ba998cd7a960771a9c13b85dec5d42d660ce807
EyEmilyKim surf <myue4555@gmail.com>
2025-01-05 18:48:54 +0900

    웰컴

---
+++ archive/05300/5337.js
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/5337
+/*
+웰컴
+
+Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+입력값 파일 :  'example.txt' -> 백준 제출 시 :  '/dev/stdin'
+*/
+
+console.log(`.  .   .
+  |  | _ | _. _ ._ _  _
+  |/\\|(/.|(_.(_)[ | )(/.`);
